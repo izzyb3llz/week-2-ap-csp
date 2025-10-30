d12a13223e1681b57045e1b5b936357abb786ebd
izzyb3llz <isacontreras519@gmail.com>
2025-10-30 20:14:34 +0000

    edits

--- 4_practice_prompts.py
+++ 4_practice_prompts.py
@@ -11,31 +11,55 @@ print("I love learning Python")
 # Print Practice #2
 # Write Python code that prints the sentence: Learning with 'TOTAL Python' is super fun!
 print("Learning with 'TOTAL Python' is super fun!")
+print(f"Learning with 'TOTAL Python' is super fun!")
 
 
 # Print Practice #3
 # Write Python code that prints the number 555 to the screen as a result of a mathematical expression
+num1 = 500
+num2 = 55
 print(500 + 55)
 
 ##############################################################################################################
 # Find 3 objects around the room and create variables from it,
 # Insert those variables into an f-string sentence(look at slide 22)in repl.it
-
+Object1= "Headphones"
+Object2 = "Phone"
+Object3 = "Computer" 
+print(f"I like to use my {Object1}. I also use my {Object2} and {Object3}.")
 
 # Familiarize yourself with the syntax of the print() function.
 # Print your name.
+name = "Isabella"
+print(f"My name is {name}.")
+
 # Print today's date.
+date = "10/30"
+print(f"Today is {date}.")
+
 # Print the name of your favorite movie.
-
+favorite_movie = "Wall-E" 
+print(f"My favorite movie is {favorite_movie}")
 # Print your name and age on separate lines using a single print() function.
-# Use f-strings to print a message like: "In 10 years, [Your Name] will be [Your Age + 10] years old."
+name = "Isabella"
+age = 16
 
+print(f"My name is {name}, and I am {age}.")
+
+# Use f-strings to print a message like: "In 10 years, [Your Name] will be [Your Age + 10] years old."
+num1 = 16
+num2 = 10
+print(f"In 10 years, {name} will be 26")
 ##############################################################################################################
 
 ###########################String Practice##################################
 #syntax is the way we write code
 # print("Hello World")
+print("Hello world!")
+
 # name = "John"
+name = "John"
+
 #in other languages, this is different
 # in javascript for example, you define
 #variables with let or const or var
